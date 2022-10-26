214f0ae249592dbbe95900bd562fbeab968e221c
hoangthiyen0903 <114420795+hoangthiyen0903@users.noreply.github.com>
2022-10-26 23:14:54 +0700

    bai4

---
+++ bai4.php
@@ -0,0 +1,23 @@
+<?php
+// Your code here!
+$mau = 'màu xanh';
+  
+if ($mau == 'màu xanh'){
+    echo 'Đây là màu xanh';
+}
+
+
+else if ($mau == 'màu đỏ')
+{
+    echo "Đây là màu đỏ";
+}
+
+if ($mau == 'màu vàng'){
+    echo 'Đây là màu vàng';
+}
+else{
+    echo 'Các màu khác';
+}
+
+
+?>
